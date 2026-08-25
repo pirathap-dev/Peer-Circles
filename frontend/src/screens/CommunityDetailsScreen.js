e1f372b94290c97d55df8ac9071572806b0c11e9
Abishekvarshan <varsityabi@gmail.com>
2026-08-25 21:47:33 +0530

    PC-49 Frontend - Implement support group details UI

--- frontend/src/screens/CommunityDetailsScreen.js
+++ frontend/src/screens/CommunityDetailsScreen.js
@@ -6,7 +6,7 @@ import { api } from '../services/api';
 import { useAuth } from '../context/AuthContext';
 import Button from '../components/Button';
 import Loader from '../components/Loader';
-// CommunityDetailsScreen.js
+// view support group
 export default function CommunityDetailsScreen() {
   const navigation = useNavigation();
   const route = useRoute();
